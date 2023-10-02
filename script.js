062c6ec86dc523477b8319d308edcbc098788fa4
AlmogAsrass <91328988+AlmogAsrass@users.noreply.github.com~>
2023-10-03 00:33:54 +0300

    added operate function.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 3;
+let secondNum = 2;
+let operator = add;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, firstNum, secondNum) {
+    return operator(firstNum, secondNum);
 }
