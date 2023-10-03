6e93d145fbe21fee8d7690b8b59e843c322323b1
AlmogAsrass <91328988+AlmogAsrass@users.noreply.github.com~>
2023-10-03 03:59:05 +0300

    add button interface to html and add functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNum = 3;
-let secondNum = 2;
-let operator = add;
+let firstNum;
+let secondNum;
+let operation;
 
 function add(a, b) {
     return a + b;
@@ -18,6 +18,53 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, firstNum, secondNum) {
-    return operator(firstNum, secondNum);
+function operate(operation, firstNum, secondNum) {
+    return operation(firstNum, secondNum);
 }
+
+const btn = document.querySelectorAll('button')
+const display = document.querySelector('#display')
+const numbers = document.querySelectorAll('.numbers')
+const operators = document.querySelectorAll('.operators')
+const result = document.querySelector('#equals')
+let displayValue = "";
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if (!operation) {
+            displayValue += number.textContent;
+            display.textContent = displayValue;
+            firstNum = Number(displayValue);
+        } else {
+            displayValue += number.textContent;
+            display.textContent = displayValue;
+            secondNum = Number(displayValue);
+        }
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        displayValue = "";
+        if (secondNum && operation) {
+            display.textContent = operate(operation, firstNum, secondNum);
+            firstNum = Number(display.textContent);
+            secondNum = undefined;
+        }
+        if (operator.textContent === "+") {
+            operation = add
+        } else if (operator.textContent === "-") {
+            operation = subtract
+        } else if (operator.textContent === "*") {
+            operation = multiply
+        } else if (operator.textContent === "/") {
+            operation = divide
+        }
+    })
+})
+
+result.addEventListener('click', () => {
+    display.textContent = operate(operation, firstNum, secondNum);
+    firstNum = Number(display.textContent);
+    secondNum = undefined;
+})
